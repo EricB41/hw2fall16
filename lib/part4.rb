3b699263b4b2ad7b4c380b2c0bb87703fe449db4
Eric B <ericbechtold4@hotmail.com>
2016-09-14 05:04:31 +0000

    Part 5 works most of the way, but still doesn't initialize to an empty array at the start like it should

--- lib/part4.rb
+++ lib/part4.rb
@@ -1,10 +1,36 @@
+#DISCLAIMER: Used the code for guidance: http://stackoverflow.com/questions/9561072/ruby-using-class-eval-to-define-methods
+
+
 class Class
 
   def attr_accessor_with_history(attr_name)
     attr_name = attr_name.to_s   # make sure it's a string
     attr_reader attr_name        # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    class_eval "YOUR CODE HERE, USE %Q FOR MULTILINE STRINGS"
-
+    instance_variable_set "@#{attr_name}_history", [attr_name]
+    class_eval %Q"
+      @#{attr_name}_history = []
+      def #{attr_name}=(newVal)
+        if !defined? @#{attr_name}_history
+          @#{attr_name}_history = []
+          @#{attr_name}_history.push(nil)
+        end
+        @#{attr_name}_history.push(newVal)
+        @#{attr_name} = newVal
+      end
+      "
   end
 end
+
+class Foo
+  attr_accessor_with_history :bar
+  
+  def initialize()
+    
+  end
+end
+
+f = Foo.new
+f.bar= 1
+f.bar= 2
+f.bar_history
--- lib/part5.rb
+++ lib/part5.rb
@@ -1,6 +1,17 @@
 class CartesianProduct
   include Enumerable
-
-  # YOUR CODE HERE
+  
+  def initialize(seq1, seq2)
+    @cartesian = Array.new
+    seq1.each do |curr1|
+      seq2.each do |curr2|
+        @cartesian.push([curr1, curr2])
+      end
+    end
+  end
+  
+  def each
+    @cartesian.each {|curr_element| yield curr_element}
+  end
 
 end
